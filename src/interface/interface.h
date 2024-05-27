97cf6819a0ff3f45b19e1cd6e8ca5a43bad5f4e6
guilherme-romanholo <guilhermeromanholobofo@gmail.com>
2024-05-27 17:48:17 -0300

    fix: ncurses interface working

--- src/interface/interface.h
+++ src/interface/interface.h
@@ -1,44 +1,45 @@
 #ifndef SISTOP_INTERFACE_H
 #define SISTOP_INTERFACE_H
 
-#define INPUT_SIZE 10
-#define SLEEP_TIME 500
-
-#include "../process/process.h"
 #include <ncurses.h>
-#include <semaphore.h>
-#include <pthread.h>
+
+#define INPUT_SIZE 20
+
+#define KERNEL_MEMORY_FMT "Remaining Memory: %d bytes."
+#define KERNEL_SEMAPH_FMT "Semaphores: %s"
+
+#define PROC_INTERFACE_FMT "PID %d, Name: %s, Seg ID: %d, State: %s, Priority: %d, PC: %d"
+#define MEMORY_INTERFACE_FMT "Segment ID: %d, Segment Size: %d Kb, Num Pages: %d."
+
+#define SCHED_PRINT_FMT "PID: %d, Print: %d ut, Remaing quantum: %d ut."
+#define SCHED_WRITE_FMT "PID: %d, Write: %d ut, Remaing quantum: %d ut."
+#define SCHED_READ_FMT "PID: %d, Read: %d ut, Remaing quantum: %d ut."
+#define SCHED_EXEC_FMT "PID: %d, Exec: %d ut, Remaing quantum: %d ut."
+#define SCHED_SEMV_FMT "PID: %d, Release Semaph %c, Remaing quantum: %d ut."
+#define SCHED_SEMP_FMT "PID: %d, Request Semaph %c, Remaing quantum: %d ut."
+#define SCHED_SEM_BLOCK_FMT "PID: %d, Blocked by Semaph %c."
+#define SCHED_QUANTUM_TIMEOUT_FMT "PID: %d, Quantum timeout."
+
 
 typedef struct {
-    // Windows
     WINDOW *window;
     WINDOW *subwindow;
-
-    // Lines
     int max_lines;
     int curr_line;
-
-    // Threads
-    sem_t mutex;
-    pthread_t thread;
 } Interface;
 
-extern Interface *kernel_interface;
-extern Interface *memory_interface;
-extern Interface *process_interface;
-extern Interface *scheduler_interface;
+extern Interface *sched_win;
 
 void Interface__init();
-void Interface__destroy(Interface *);
-Interface *Interface__create_window(int height, int width, int starty, int startx, char *title, int scroll);
-void Interface__add_line(Interface *interface, int width, const char *text);
-void Interface__input_menu(char *input);
-void Interface__update_kernel_window();
-void Interface__kernel_semaphores();
-void Interface__update_memory_window();
-void Interface__update_process_window();
+Interface *Interface__create_window(int, int, int, int, char *, int);
+void Interface__delete_window(Interface *window);
+void Interface__add_line(Interface *, int, const char *);
+void Interface__input_menu(char *);
+void Interface__send_data(Interface *, const char *, ...);
+void Interface__update();
+void Interface__refresh_kernel_win();
+void Interface__refresh_process_win();
 char *Interface__cast_process_state(int);
-void Interface__add_scheduler_log(int pid, Instruction *instr, int quantum);
-char *Interface__cast_instruction_opcode(Instruction *);
+void Interface__refresh_memory_win();
 
 #endif //SISTOP_INTERFACE_H
--- src/main.c
+++ src/main.c
@@ -2,17 +2,20 @@
 #include "interface/interface.h"
 #include "scheduler/scheduler.h"
 #include "process/process.h"
+#include <pthread.h>
+
 int main(int argc, char *argv[])
 {
     pthread_t sched;
+
     pthread_create(&sched,
                    NULL,
                    (void *) Scheduler__cpu_run,
                    NULL);
 
     Kernel__create();
-    Interface__init();
 
+    Interface__init();
 
     return 0;
 }
